a21a5ab585e95805829b1b0796cf3c96444735fc
Christina Gonzalez <cmgamway@gmail.com>
2019-08-07 08:25:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,25 +1,29 @@
-def addition(num1 = 5,num2 = 4)
+def addition(num1="5", num2="4")
   when "adds two numbers together" do
 
 end
 
-def subtraction(num1 = 10,num2 = 5)
+def subtraction(num1="10", num2="5")
   when "subtracts two numbers from each other" do
 
 end
 
-def division(num1, num2)
-
+def division(num1="50", num2="2")
+  when"divides one number by another" do
+    
 end
 
-def multiplication(num1, num2)
-
+def multiplication(num1="4", num2="30")
+  when "multiplies two numbers together" do
+    
 end
 
-def modulo(num1, num2)
-
+def modulo(num1="34", num2="5")
+  when "returns the remainder of two numbers" do
+    
 end
 
-def square_root(num)
-
+def square_root(num="81")
+  when "returns the square root of a number" do
+    
 end
